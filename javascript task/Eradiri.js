0e6fdb7486dd93d760060e4f9b9bdf2a305bcf43
Perebonilado <86803872+Perebonilado@users.noreply.github.com>
2021-08-20 08:36:44 +0000

    initial commit

---
+++ javascript task/Eradiri.js
@@ -0,0 +1,7 @@
+let fullName = "Eradiri Perebonilado Richard"
+
+console.log(`My name is ${fullName}.`);
+
+function myName() {
+    document.getElementById("myname").innerHTML = `My name is <b>  ${fullName}</b>.`
+}
